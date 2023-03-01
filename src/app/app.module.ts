1258c46ce6d815b1883c6e895290c1fc06f05f82
Jemmal soufiane <jemmalsoufiane@gmail.com>
2023-03-01 21:28:52 +0100

    adding folders and navbar initial tags

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BlogsComponent } from './pages/blogs/blogs.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { StoreComponent } from './pages/store/store.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    BlogsComponent,
+    AdminComponent,
+    StoreComponent
   ],
   imports: [
     BrowserModule,
